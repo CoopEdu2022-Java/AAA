196d7ef896cff2af3fdf7b24683a7842759cad8a
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-09-07 02:09:32 +0800

    submit

--- chapter01/java122.java
+++ chapter01/java122.java
@@ -1,7 +1,26 @@
-package chapter01;
 
 public class java122 {
+
     public static void main(String[] args) {
-        
+        int count = 0;
+        for (int i = 101; i < 201; i++) {// 这里描述了要求解的素数的问题
+            boolean s = false;
+            // boolean的作用就是记录题目中是否存在素数，一次循环下来若是素数，这s为true。
+            for (int j = 2; j <= Math.sqrt(i); j++) {// 从2开始到这个数的平方根j进行运算，看是否存在其他因数
+                if (i % j == 0) {
+                    s = false;
+                    break;
+                } else {
+                    s = true;
+                }
+            }
+            if (s == true) {// 利用true这个结构来统计count的数量
+                count++;
+
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\n" + "素数总数为：" + count);
     }
+
 }
