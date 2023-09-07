f18ff461d2739a99a58926ebdf363de99d51769b
laladoko <hongsen.xu.2001@stu.moonshotacademy.cn>
2023-09-07 09:55:06 +0800

    all

---
+++ chapter01/java127.java
@@ -0,0 +1,13 @@
+
+
+public class java127 {
+    public static void main(String[] args){
+       float height = 100;
+       int count =10;
+       for (int i = 0; i < count; i++) {
+        height = height/2;
+        System.out.println(height);
+        
+       } 
+    }
+}
